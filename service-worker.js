e7bcdb5bb7116ab118128f9fa0a471bce4df8e7b
yukiamiya1-debug <yukiamiya1@gmail.com>
2026-05-25 13:37:36 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,62 @@
-const CACHE_NAME = "asset-dashboard-v1";
+const CACHE_NAME = "asset-dashboard-v2";
 
 const urlsToCache = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
+
+  self.skipWaiting();
+
   event.waitUntil(
+
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+
   );
 });
 
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+
+        keys.map(key => {
+
+          if(key !== CACHE_NAME){
+            return caches.delete(key);
+          }
+
+        })
+
+      );
+
+    })
+
+  );
+
+  self.clients.claim();
+
+});
+
 self.addEventListener("fetch", event => {
+
   event.respondWith(
+
     caches.match(event.request)
       .then(response => {
+
         return response || fetch(event.request);
+
       })
+
   );
+
 });
